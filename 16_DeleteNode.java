060b623fcca6ba2e9a9ef0f54b56133c4b5aeae5
AssadullahMalik <asadullahmk102@gmail.com>
2026-02-25 12:44:47 +0500

    Update 16_DeleteNode.java

--- 16_DeleteNode.java
+++ 16_DeleteNode.java
@@ -1,16 +1,29 @@
-class InsertAtBeginning {
-    static Node insert(Node head, int data) {
-        Node newNode = new Node(data);
-        newNode.next = head;
-        return newNode;
+class DeleteNode {
+    static Node delete(Node head, int key) {
+        if (head == null) return null;
+        if (head.data == key) return head.next;
+
+        Node temp = head;
+        while (temp.next != null && temp.next.data != key)
+            temp = temp.next;
+
+        if (temp.next != null)
+            temp.next = temp.next.next;
+
+        return head;
     }
 
     public static void main(String[] args) {
-        Node head = new Node(20);
-        head = insert(head, 10);
+        Node head = new Node(10);
+        head.next = new Node(20);
+        head.next.next = new Node(30);
+
+        head = delete(head, 20);
+
         while (head != null) {
             System.out.print(head.data + " ");
             head = head.next;
         }
     }
 }
+
