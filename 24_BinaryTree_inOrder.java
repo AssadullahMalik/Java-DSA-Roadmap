c483a74faa3b0263ed49a3dcf68b5f15461e5930
AssadullahMalik <asadullahmk102@gmail.com>
2026-02-25 12:47:18 +0500

    Update 24_BinaryTree_inOrder.java

---
+++ 24_BinaryTree_inOrder.java
@@ -0,0 +1,16 @@
+public class BinaryTreeInorder {
+    static void inorder(TreeNode root) {
+        if (root != null) {
+            inorder(root.left);
+            System.out.print(root.data + " ");
+            inorder(root.right);
+        }
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        inorder(root);
+    }
+}
