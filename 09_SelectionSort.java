a7f7696824276fe44892dc74c6ac47393e1fffa9
AssadullahMalik <asadullahmk102@gmail.com>
2026-02-25 12:40:23 +0500

    Update 09_SelectionSort.java

---
+++ 09_SelectionSort.java
@@ -0,0 +1,18 @@
+public class SelectionSort {
+    public static void main(String[] args) {
+        int[] arr = {4, 2, 7, 1};
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            int min = i;
+            for (int j = i + 1; j < arr.length; j++)
+                if (arr[j] < arr[min]) min = j;
+
+            int temp = arr[min];
+            arr[min] = arr[i];
+            arr[i] = temp;
+        }
+
+        for (int i : arr)
+            System.out.print(i + " ");
+    }
+}
